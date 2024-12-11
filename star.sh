925499e978e12a361c1ef3e2e082f3f18840164c
Sachin_07 <106669438+Sachinpawar86@users.noreply.github.com>
2024-12-11 19:14:26 +0530

    Update star.sh

--- star.sh
+++ star.sh
@@ -11,7 +11,7 @@ rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
 
 # Initialize ROM manifest
-repo init -u https://github.com/VoltageOS/manifest.git -b 15 --git-lfs
+repo init -u https://github.com/Project-PixelStar/manifest -b 14-qpr3 --git-lfs
 
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
@@ -20,21 +20,24 @@ repo init -u https://github.com/VoltageOS/manifest.git -b 15 --git-lfs
 # rm -rf frameworks/native
 
 # cloning device tree
-git clone https://github.com/VoltA155/device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
-git clone https://github.com/VoltA155/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
+git clone https://github.com/PixelStarA14/device_xiaomi_mojito.git --depth 1 -b 15 device/xiaomi/mojito
+git clone https://github.com/PixelStarA14/android_device_xiaomi_sm6150-common.git --depth 1 -b 15 device/xiaomi/sm6150-common
 
 # cloning kernel tree
-git clone https://github.com/VoltA155/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
+git clone https://github.com/PixelStarA14/kernel_xiaomi_mojito.git --depth 1 -b inline-rom kernel/xiaomi/mojito
 
 # cloning vendor tree
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_mojito.git --depth 1 -b 14 vendor/xiaomi/mojito
 git clone https://gitlab.com/sachinbarange86/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 
 # cloning hardware tree
-git clone https://github.com/VoltA155/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
+git clone https://github.com/PixelStarA14/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
 # Set up the build environment
 . build/envsetup.sh
 
 # Choose the target device
-brunch mojito
+lunch pixelstar_mojito-userdebug
+
+#bacon
+mka bacon -jXT
